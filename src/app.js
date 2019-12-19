73a08212255c1983ad405a5164b7caafea249a40
BassamAzizi <101010bassam@gmail.com>
2019-12-19 22:03:57 +0100

    creating components and styling them

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import "./app.css";
+
+const Intro = props => (
+  <p className="App-intro">Our first functional component</p>
+);
 
 class App extends Component {
   render() {
-    return <h1> THIS IS the first react component </h1>;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tv-Series-App</h1>
+        </header>
+        <Intro />
+      </div>
+    );
   }
 }
 
